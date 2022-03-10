783f7abe824da608f4f4ebd1e71e930480628fed
kanger-007 <67798221+kanger-007@users.noreply.github.com>
2022-03-10 13:48:18 +0530

    Update dl.py

--- dl.py
+++ dl.py
@@ -19,7 +19,7 @@ realPath = os.path.realpath(currentFile)
 dirPath = os.path.dirname(realPath)
 dirName = os.path.basename(dirPath)
 
-ytdlp = dirPath + '/binaries/yt-dlp'
+ytdlp = dirPath + '/binaries/ytdlp'
 aria2c = dirPath + '/binaries/aria2c.exe'
 mp4decrypt = dirPath + '/binaries/mp4decrypt.exe'
 mkvmerge = dirPath + '/binaries/mkvmerge.exe'
